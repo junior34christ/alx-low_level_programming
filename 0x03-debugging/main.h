efc60cdaf56a441d4d8acae5562b31570bb1ce02
junior252christ <junior252christ@gmail.com>
2022-09-15 00:48:32 +0000

    first commit

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+# ifndef MAIN_H
+# définir  MAIN_H
+
+# inclure  < stdio.h >
+
+void  positive_or_negative ( int i);
+int  plus grand_nombre ( int a, int b, int c);
+int  convert_day ( int mois, int jour);
+void  print_remaining_days ( int month, int day, int year);
+
+# endif  /* MAIN_H */
