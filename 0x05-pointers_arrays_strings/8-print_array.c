6d123bc392188e8ff9b5bb76c7394540296fea08
junior252christ <junior252christ@gmail.com>
2022-09-19 15:57:02 +0000

    My solution of the code C

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,27 +1,21 @@
 #include "main.h"
-
 /**
- * print_array - print n element of array
- * @a: array
- * @n: number of elements
- * Return: nothing
- *
+ *print_array - print n element of array
+ *@a: array
+ *@n: number of elements
+ *Return: nothing
  */
-
 void print_array(int *a, int n)
 {
 	int inc;
 
-	for (inc = 0; inc < n; inc++)
+	for (inc = 0; inc < n ; inc++)
 	{
 		if (inc != n - 1)
-		{
-			printf("%d", a[inc]);
-		}
+			printf("%d, ", a[inc]);
 		else
-		{
 			printf("%d", a[inc]);
-		}
+
 	}
 	printf("\n");
 }
