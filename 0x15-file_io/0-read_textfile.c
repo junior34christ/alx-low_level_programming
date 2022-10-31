f8a380081c858d38a139517f652d593009aa4663
junior34christ <jeanchrist252@gmail.com>
2022-10-31 13:55:18 +0000

    Solution of the code C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,47 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
 
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letters
- * Description: Write a function that reads a text file and prints it
- * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: file to read
+ * @letters: number of letters it should read and print
+ * Return: actual numbers it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
-
-	text = malloc(letters);
-	if (text == NULL)
-		return (0);
+	int fd, fd_read, fd_write;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
 	{
-		free(text);
+		free(buff);
 		return (0);
 	}
-
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
-
-	close(file);
-
-	return (w);
+	fd_read = read(fd, buff, letters);
+	if (fd_read == -1)
+		return (0);
+	fd_write = write(STDOUT_FILENO, buff, fd_read);
+	if (fd_write == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	if (fd_read != fd_write)
+		return (0);
+	free(buff);
+	close(fd);
+	return (fd_write);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,14 +1,6 @@
-#ifndef FILE_IO
-#define FILE_IO
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int _putchar(char c);
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-#endif
+#endif /* #ifndef _HOLBERTON_H */
