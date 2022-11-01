812c14e5b2282b6476f18d35d47c160e9a271f95
junior34christ <jeanchrist252@gmail.com>
2022-11-01 08:14:03 +0000

    Pull solution of the code C

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,9 +4,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-void close_fd(int fd);
-void copy_contents(int from_fd, int to_fd, char *dest_file);
+#include "main.h"
 
 /**
  * main - Copies the content of a file to another file
@@ -19,21 +17,16 @@ void copy_contents(int from_fd, int to_fd, char *dest_file);
 int main(int argc, char *argv[])
 {
 	int from_fd, to_fd;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	to_fd = open(argv[2], O_WRONLY | O_TRUNC);
+	to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (to_fd < 0)
 	{
-		to_fd = open(argv[2], O_WRONLY | O_CREAT, 0664);
-		if (to_fd < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	from_fd = open(argv[1], O_RDONLY);
 	if (from_fd < 0)
@@ -42,11 +35,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	copy_contents(from_fd, to_fd, argv[2]);
+	copy_contents(from_fd, to_fd, argv[1], argv[2]);
 	close_fd(from_fd), close_fd(to_fd);
 	return (0);
 }
-
 /**
  * close_fd - closes a file handle and exits program on failure
  * @fd: The file handle
@@ -59,18 +51,17 @@ void close_fd(int fd)
 		exit(100);
 	}
 }
-
 /**
  * copy_contents - Copies the contents from one file to another
  * @from_fd: The source file handle
  * @to_fd: The destination file handle
+ * @src_file: The source file name
  * @dest_file: The destination file name
  */
-void copy_contents(int from_fd, int to_fd, char *dest_file)
+void copy_contents(int from_fd, int to_fd, char *src_file, char *dest_file)
 {
 	int i, c, buf_size = 1024;
 	void *buf = malloc(sizeof(char) * buf_size);
-
 	if (buf != NULL)
 	{
 		for (i = 0; ; i += buf_size)
@@ -78,9 +69,15 @@ void copy_contents(int from_fd, int to_fd, char *dest_file)
 			c = read(from_fd, buf, buf_size);
 			if (c == 0)
 				break;
+			if (c < 0)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
+				exit(98);
+			}
 			if (write(to_fd, buf, c) != c)
 			{
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
+				free(buf);
 				exit(99);
 			}
 		}
